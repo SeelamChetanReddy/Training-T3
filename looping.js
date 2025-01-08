344b72fcf9215560dadce9a54149757fe12c389c
SeelamChetanReddy <2211cs010573@mallareddyuniversity.ac.in>
2025-01-08 10:26:45 +0530

    pushing the files to repo

---
+++ looping.js
@@ -0,0 +1,54 @@
+// //Loops
+
+// //for loop
+// for(let i=1;i<5;i++){
+//     console.log("good Morning");
+// } 
+// for(let a=0;a<=100;a++){
+//     if(a%2==0){
+//         console.log(a +" is a even number");
+//     }else{
+//         console.log(a + " is a odd number");
+//     }
+// }
+
+// //while loop
+
+// let i=1;
+// while(i<=5){
+//     console.log("Good morning!!"); 
+//     i++;
+// }  
+
+// //do while loop
+
+// let i=1;
+// let n=5;
+// do{
+//     console.log("Batch 3 is Awesome , they are responding well");
+//     i++;
+// }while(i<=n);
+
+// // break and continue
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+//     if(i==5){
+//         break;
+//     }
+// }
+
+// for(let i=1;i<=10;i++){
+//     if(i==5){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+const addToCart = () => {
+    alert("Item " + + " added to cart");
+}
+
+function add(a, b) {
+    console.log(a + b);
+}
+add(10, 20);

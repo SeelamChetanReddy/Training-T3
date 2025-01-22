c3740d3aba118e1c459c6a291b68863388583178
SeelamChetanReddy <2211cs010573@mallareddyuniversity.ac.in>
2025-01-22 12:54:42 +0530

    pushing the files to repo

--- async.js
+++ async.js
@@ -16,13 +16,40 @@
 
 // let p1 = new Promise((resolve, reject) => {});
 
-let p2 = new Promise((resolve, reject) => {
-        resolve("Success");
-});
+// let p2 = new Promise((resolve, reject) => {
+//         resolve("Success");
+// });
 
-p2.then((res) => console.log(res)).catch((err) => console.log(err)).finally(() => console.log("Finally for both"));
+// p2.then((res) => console.log(res)).catch((err) => console.log(err)).finally(() => console.log("Finally for both"));
 
-let p3 = new Promise((resolve, reject) => {
-        reject("Failure");
-});
-p3.then((res) => console.log(res)).catch((err) => console.log(err)).finally(() => console.log("Finally for both"))
+// let p3 = new Promise((resolve, reject) => {
+//         reject("Failure");
+// });
+// p3.then((res) => console.log(res)).catch((err) => console.log(err)).finally(() => console.log("Finally for both"))
+
+// API fetching
+
+function fetchUser() {
+        let response = fetch("https://jsonplaceholder.typicode.com/users");
+        // console.log(response);
+        response.then(result =>{ 
+                // console.log(res.json());
+                return result.json().then(data => {
+                        // console.log(data)
+                        let store = document.getElementById("store");
+                        data.map(user => {
+                                store.innerHTML += `
+                                <tr>
+                                <td>${user.id}</td>
+                                <td>${user.name}</td>
+                                <td>${user.email}</td>
+                                <td>${user.company.name}</td>
+                                </tr>
+                                `
+                        })
+                })
+        })
+        .catch(err => console.log(err));
+}
+fetchUser();
+

ad3d8a4f6e29fe92ebd94db88e9375374f36082f
SeelamChetanReddy <2211cs010573@mallareddyuniversity.ac.in>
2025-01-25 12:56:44 +0530

    pushing the files to repo

--- dom.js
+++ dom.js
@@ -28,18 +28,14 @@
 // let ele = document.getElementById("mru");
 // console.log(ele);
 
-
-
 // let ele = document.querySelector("#mru");
 // console.log(ele);
 
 // let ele = document.querySelector(".test");
 // console.log(ele);
 
-
 // let ele = document.querySelectorAll(".test");
 // console.log(ele);
-
 
 // let btn = document.querySelector('#btn');
 // btn.addEventListener('click', () => {
@@ -53,14 +49,60 @@
 //     console.log('Button Double clicked');
 // })
 
-let divBlock = document.getElementById('divBlock');
-divBlock.addEventListener('onMouseOver', () => {
-    document.body.style.backgroundColor = 'yellow';
-    divBlock.style.backgroundColor = 'blue';
-})
+// let divBlock = document.getElementsByClassName('divBlock');
+// divBlock.addEventListener('onMouseOver', () => {
+//     document.body.style.backgroundColor = 'yellow';
+//     divBlock.style.backgroundColor = 'blue';
+// })
 
-divBlock.addEventListener('onMouseLeave', () => {
-    document.body.style.backgroundColor = 'yellow';
-    divBlock.style.backgroundColor = 'blue';
-})
+// divBlock.addEventListener('onMouseLeave', () => {
+//     document.body.style.backgroundColor = 'yellow';
+//     divBlock.style.backgroundColor = 'blue';
+// })
 
+// let bgColor = document.querySelector('.bgColor');
+
+// [...bgColor].map(element => {
+//     element.addEventListener('onMouseOver', () => {
+//         element.style.backgroundColor = 'blue';
+// })
+// })
+
+// let ele = document.createElement("h1");
+// ele.textContent = "Hello, World!";
+// ele.style.color = 'red';
+// ele.id = 'demo';
+// document.body.appendChild(ele);
+
+// console.log(ele);
+
+// let img = document.createElement("img");
+// img.src = "./Ro.jpg";
+// img.style.width = "1200px";
+// img.style.height = "500px";
+// img.style.position = "absolute";
+// img.style.transition = "all 1s ease-in-out";
+// console.log(img);
+// document.body.appendChild(ele);
+// document.body.appendChild(img);
+
+let form = document.querySelector("form");
+let username = document.getElementById("uName");
+let email = document.getElementById("uMail");
+let password = document.getElementById("uPass");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // console.log(event);
+  // console.log("Form submitted");
+  let uname = username.value;
+  let uemail = email.value;
+  let upass = password.value;
+  let userDetails = {
+    uname,
+    uemail,
+    upass,``
+  };
+  console.log(userDetails);
+  localStorage.setItem("userDetails", JSON.stringify(userDetails));
+});

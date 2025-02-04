6952b46e34f8843f776fe089d1f0512939799828
SeelamChetanReddy <2211cs010573@mallareddyuniversity.ac.in>
2025-02-04 12:59:58 +0530

    pushing the files to repo

--- React/training/src/App.js
+++ React/training/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
 
-function App() {
+// function App() {
+//   // let ele = React.createElement("div",{className : "App"},[
+//   //   React.createElement("h1",null,"Hello, world!"),
+//   // ])
+//   let user = "sunny";
+//   return (
+    
+//     <div className="App">
+//       <h1 style={{color:"red"}}>HI</h1>
+//       <h1>Chetan</h1>
+//       <h1>{user}</h1>
+//       <h1>{28+12}</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// ! componenet composition
+
+
+import React from 'react';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Sidebar1 from './components/Sidebar1';
+import Sidebar2 from './components/Sidebar2';
+import Footer from './components/Footer';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar/>
+      <Main/>
+      <div className='sidebar'>
+      <Sidebar1/>
+      <Sidebar2/>
+      </div>
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
